ab2f180aff545b9d56c88843e4b5ba7b0cd0fa97
ambrouse <thcihthichich3568@gmail.com>
2025-08-11 00:10:20 +0700

    11-8-2025

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture(0)  # 0 là camera mặc định
-
-if cap.isOpened():
-    print("Camera sẵn sàng và không bị chiếm dụng.")
-    cap.release()
-else:
-    print("Camera đang bị chiếm dụng hoặc không thể truy cập.")
